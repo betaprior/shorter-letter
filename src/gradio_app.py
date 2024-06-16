b7d8e9396874e81b13e5bb67ea699b97142df318
Vladimir Iglovikov <iglovikov@gmail.com>
2024-06-15 18:09:04 -0700

    Updated styles

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 import json
-from functions_old import create_highlight_structure
+from src.functions_old import create_highlight_structure
 import html2text
 import requests
 
@@ -8,11 +8,32 @@ h = html2text.HTML2Text()
 h.ignore_links = True
 
 STYLE_DEFINITIONS = {
-    "noun": "color: black; font-weight: bold;",
-    "adjective": "color: gray;",
-    "verb": "color: black; font-weight: bold;",
-    "stop_word": "color: gray;",
+    "noun": "color: navy; font-weight: bold;",      # Dark blue for better contrast
+    "adjective": "color: orange;",                  # Medium orange for visibility
+    "verb": "color: red; font-weight: bold;",       # Bright red for emphasis
+    "stop_word": "color: lightgray;",               # Light gray to deemphasize stop words
 }
+
+
+
+custom_css = """
+body {
+    font-family: 'Verdana', sans-serif;
+    font-size: 16px; /* Adjust the size as needed */
+    color: #333333; /* Optional: Adjust the color if needed */
+}
+
+p {
+    font-family: 'Verdana', sans-serif;
+    font-size: 24px; /* Adjust the size as needed */
+    color: #333333; /* Optional: Adjust the color if needed */
+}
+
+em {
+    font-style: italic;
+    color: #888888; /* Optional: Adjust the color if needed */
+}
+"""
 
 precomputed_styles = None
 text_content = None
@@ -43,21 +64,21 @@ def fetch_text_and_apply_styles(url, selected_styles):
         response = requests.get(url)
         text_content = h.handle(response.text)
         precomputed_styles = create_highlight_structure(text_content)
-    
+
     if precomputed_styles is None or text_content is None:
         return "", gr.update(visible=True)
-    
+
     styled_text = apply_styles(text_content, precomputed_styles, STYLE_DEFINITIONS, selected_styles)
     return styled_text, gr.update(visible=True)
 
 def update_styles(selected_styles):
     if precomputed_styles is None or text_content is None:
         return ""
-    
+
     styled_text = apply_styles(text_content, precomputed_styles, STYLE_DEFINITIONS, selected_styles)
     return styled_text
 
-with gr.Blocks(theme='JohnSmith9982/small_and_pretty') as demo:
+with gr.Blocks(theme='JohnSmith9982/small_and_pretty', css=custom_css) as demo:
     user_input = gr.Textbox(value="https://www.paulgraham.com/die.html", label="User Input", placeholder="Enter url here...")
     run_button = gr.Button(value="Run")
     style_checkbox = gr.CheckboxGroup(label="Select Styles to Apply", choices=["noun", "adjective", "verb", "stop_word"], value=["noun", "adjective", "verb", "stop_word"], visible=False)
